2eb4141eea1ef15983095e6446f6116d3b43513c
Boris Savushkin <savbopa@gmail.com>
2025-12-16 14:49:06 +0200

    Improve filename uniqueness check in resolver

--- src/processors/filename_resolver.py
+++ src/processors/filename_resolver.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 from typing import Set
 
+
 class FileNameResolver:
     def __init__(self, base_folder: Path):
         self.used_names: Set[str] = set()
         self._seed_existing_files(base_folder)
 
-
     def _seed_existing_files(self, folder: Path) -> None:
         try:
-            self.used_names = {path.name for path in folder.iterdir() if path.is_file()}
+            self.used_names = {
+                path.name for path in folder.iterdir() if path.is_file()}
         except Exception:
             self.used_names = set()
-
 
     def resolve_unique_path(self, path: Path) -> Path:
         base_name = path.stem
@@ -22,7 +22,7 @@ class FileNameResolver:
         candidate_file_path = path
         suffix = 1
 
-        while candidate_file_path.name in self.used_names:
+        while candidate_file_path.name in self.used_names or candidate_file_path.exists():
             candidate_file_path = parent / f"{base_name}_{suffix}{extension}"
             suffix += 1
 
