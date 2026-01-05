0459ddecd3eedb57a4464158111eb1a4fcf0f00b
Boris Savushkin <savbopa@gmail.com>
2026-01-05 11:34:31 +0200

    BUGFIX: Remove non-existing import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.config.main import Config
 from src.downloader import MemoryDownloader
 from src.logger.log_initializer import LogInitializer
 from src.logger.log import log
-from src.error_handling import handle_app_errors
 
 if __name__ == "__main__":
     config = Config.from_args()
@@ -12,7 +11,6 @@ if __name__ == "__main__":
 
     downloader = MemoryDownloader(config)
 
-    @handle_app_errors(exit_code_on_error=1)
     def run_app(downloader):
         downloader.run()
 
