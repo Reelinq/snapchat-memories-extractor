bfc7b01b3559fd5868fa12e00e36dea0d1d00ff0
Boris Savushkin <savbopa@gmail.com>
2026-01-06 12:50:16 +0200

    Remove unused config options from metadata writers

--- src/metadata/metadata_dispatcher.py
+++ src/metadata/metadata_dispatcher.py
@@ -7,10 +7,7 @@ from src.metadata.video_metadata_writer import VideoMetadataWriter
 
 class MetadataDispatcher:
     def write_metadata(self, memory: Memory, file_path: Path, is_image: bool):
-        ffmpeg_timeout = Config.from_args().cli_options['ffmpeg_timeout']
-        jpeg_quality = Config.from_args().cli_options['jpeg_quality']
-
         if is_image:
-            ImageMetadataWriter().write_image_metadata(memory, file_path, jpeg_quality)
+            ImageMetadataWriter().write_image_metadata(memory, file_path)
         else:
-            VideoMetadataWriter().write_video_metadata(memory, file_path, ffmpeg_timeout)
+            VideoMetadataWriter().write_video_metadata(memory, file_path)
