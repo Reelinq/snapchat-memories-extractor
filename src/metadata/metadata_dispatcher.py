3ed3d0705ffbad0ac1ef64b0afec476495c8d4ab
Boris Savushkin <savbopa@gmail.com>
2026-01-06 08:19:54 +0200

    Refactor metadata service to use dispatcher pattern
    Replaces the monolithic MetadataService with a new MetadataDispatcher class, delegating image and video metadata writing to specialized writer classes. This improves code organization and separation of concerns.

---
+++ src/metadata/metadata_dispatcher.py
@@ -0,0 +1,16 @@
+from src.models import Memory
+from pathlib import Path
+from src.config.main import Config
+from src.metadata.image_metadata_writer import ImageMetadataWriter
+from src.metadata.video_metadata_writer import VideoMetadataWriter
+
+
+class MetadataDispatcher:
+    def write_metadata(self, memory: Memory, file_path: Path, is_image: bool):
+        ffmpeg_timeout = Config.from_args().cli_options['ffmpeg_timeout']
+        jpeg_quality = Config.from_args().cli_options['jpeg_quality']
+
+        if is_image:
+            ImageMetadataWriter()._write_image_metadata(memory, file_path, jpeg_quality)
+        else:
+            VideoMetadataWriter()._write_video_metadata(memory, file_path, ffmpeg_timeout)
