81455104cf4ddbc1d1e7c37ad5b8caba4d8500f1
Boris Savushkin <savbopa@gmail.com>
2026-01-07 08:47:00 +0200

    Add ImageProcessor for image conversion and metadata
    Introduces the ImageProcessor class with a process_image method to handle JXL conversion and metadata writing based on configuration options.

---
+++ src/media_dispatcher/image_processor.py
@@ -0,0 +1,17 @@
+from src.models import Memory
+from pathlib import Path
+from src.config.main import Config
+from src.services.jxl_converter import JXLConverter
+from src.metadata.image_metadata_writer import ImageMetadataWriter
+
+
+class ImageProcessor:
+    def process_image(memory: Memory, file_path: Path):
+        convert_to_jxl = Config.from_args().cli_options['convert_to_jxl']
+        write_metadata = Config.from_args().cli_options['write_metadata']
+
+        if convert_to_jxl:
+            file_path = JXLConverter.convert_to_jxl(file_path)
+
+        if write_metadata:
+            ImageMetadataWriter.write_image_metadata(memory, file_path)
