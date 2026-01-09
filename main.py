9bce8f5dff0fc265c20e59b6e50b1f5c2705e2e0
Boris Savushkin <savbopa@gmail.com>
2026-01-09 20:52:19 +0200

    Refactor downloader initialization in main.py
    Replaces direct Config and MemoryDownloader instantiation with SetupDownloader and updates logger initialization. Now both SetupDownloader and MemoryDownloader are run directly, simplifying the main entry point.

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from src.config.main import Config
-from src.downloader import MemoryDownloader
+from src.downloader.setup_downloader import SetupDownloader
+from src.downloader.downloader import MemoryDownloader
 from src.logger.log_initializer import LogInitializer
 from src.logger.log import log
 
 if __name__ == "__main__":
-    config = Config.from_args()
-
-    logger = LogInitializer(config)
+    logger = LogInitializer().configure_logger()
     log("Application started", "info")
 
-    downloader = MemoryDownloader(config)
-
-    def run_app(downloader):
-        downloader.run()
-
-    run_app(downloader)
+    SetupDownloader().run()
+    MemoryDownloader().run()
