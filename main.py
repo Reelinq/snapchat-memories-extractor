03f72a744ae33ca64571387afb72db0d9044d3bd
Boris Savushkin <savbopa@gmail.com>
2026-01-10 14:09:46 +0200

    Initialize StatsManager in main entry point
    Added initialization of StatsManager in the main application entry point to ensure statistics management is set up at startup. Also removed unused logger variable.

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from src.downloader.setup_downloader import SetupDownloader
 from src.downloader.downloader import MemoryDownloader
 from src.logger.log_initializer import LogInitializer
 from src.logger.log import log
+from src.ui.stats_manager import StatsManager
 
 if __name__ == "__main__":
-    logger = LogInitializer().configure_logger()
+    LogInitializer().configure_logger()
+    StatsManager()
     log("Application started", "info")
 
     SetupDownloader().run()
