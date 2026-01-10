d703312aa48da88e23fd3ae9512fa24b4a8069d4
Boris Savushkin <savbopa@gmail.com>
2026-01-10 14:10:14 +0200

    Add UpdateUI class for refreshing display
    Introduces the UpdateUI class with a method to clear the terminal and refresh the display using the Display class. This utility will help manage UI updates in the application.

---
+++ src/ui/update_ui.py
@@ -0,0 +1,14 @@
+import sys
+from src.ui.display import Display
+
+
+class UpdateUI():
+    def run(self):
+        self._clear_display()
+        Display().print_display()
+
+
+    @staticmethod
+    def _clear_display(lines=10):
+        for _ in range(lines):
+            sys.stdout.write('\033[F\033[K')
