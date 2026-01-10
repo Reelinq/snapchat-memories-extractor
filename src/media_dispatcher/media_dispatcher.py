d2b115bde6ff615211c8a90751647ca64842c7a1
Boris Savushkin <savbopa@gmail.com>
2026-01-10 15:07:01 +0200

    Refactor image processing to use function instead of class
    Replaces the ImageProcessor class with a standalone process_image function and updates imports and usage accordingly. This simplifies the code and streamlines the image processing workflow.

--- src/media_dispatcher/media_dispatcher.py
+++ src/media_dispatcher/media_dispatcher.py
@@ -1,13 +1,13 @@
 from src.models import Memory
 from pathlib import Path
-from src.media_dispatcher.image_processor import ImageProcessor
+from src.media_dispatcher.image_processor import process_image
 from src.media_dispatcher.video_processor import process_video
 from src.media_dispatcher.zip_processor import ZipProcessor
 
 
 def process_media(memory: Memory, file_path: Path):
     if memory.media_type == "Image":
-        ImageProcessor.process_image(memory, file_path)
+        process_image(memory, file_path)
     elif memory.media_type == "Video":
         process_video(memory, file_path)
     else:
